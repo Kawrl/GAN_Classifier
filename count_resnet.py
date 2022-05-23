2afccbe2489428361ae11ac6d2383b20263d3f71
Karl <karl.griphammar@gmail.com>
2022-05-23 09:13:00 +0200

    added total

--- count_resnet.py
+++ count_resnet.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
 rest_set_path = Path('/data/rest_set')
-
+total_=0
 for label in rest_set_path.iterdir():
     label_files = [f for f in label.glob('*')]
     print(f'There are {len(label_files)} samples in label {label.stem}')
+    total_+=len(label_files)
+
+print(f'In total: {total_} files')
