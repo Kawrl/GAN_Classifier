686a33dd2c875c718b6bcd444f8558c7dc6199ce
Karl <karl.griphammar@gmail.com>
2022-05-23 13:17:15 +0200

    Added assertion

--- read_and_fill_subsets.py
+++ read_and_fill_subsets.py
@@ -1,5 +1,32 @@
 from pathlib import Path
 
+def is_there_test_leakage(dir_path):
+    print('Asserting image dir: ', dir_path)
+    test_set_path = Path('/data/test_crops')
+    total_=0
+    for label in dir_path.iterdir():
+        label_files = [f for f in label.glob('*')]
+        print(f'There are {len(label_files)} samples in label {label.stem}')
+        total_+=len(label_files)
+
+    print(f'In total: {total_} files')
+
+    all_rest_files = list(dir_path.glob('*/*'))
+    all_files = list(test_set_path.glob('*/*'))
+    assert len(all_rest_files)==total_, "Something wrong with file counting?"
+
+    all_rest_file_names = [f.name for f in all_rest_files]
+    full_file_names = [f.name for f in all_files]
+    print(len(all_rest_file_names))
+    print(len(full_file_names))
+
+    assert len(list(set(all_rest_file_names) & set(full_file_names))) == 0, \
+            f"Intersection of train and test files: {list(set(all_rest_file_names) & set(full_file_names))}"
+
+    for f in all_rest_file_names:
+        if f in full_file_names:
+            print(f)
+
 def read_file_file(file):
     '''
     Reads file containing list of file names. Returns list
@@ -53,7 +80,7 @@ if __name__ == '__main__':
         small_subset_1000 = [f for f in all_filenames if f.name in files_1000]
 
         fill_sub_dir(new_subdir_200, small_subset_200)
-        fill_sub_dir(new_subdir_1000, small_subset_200)
+        fill_sub_dir(new_subdir_1000, small_subset_1000)
         print('Done with subdir {}'.format(subdir))
 
     print('Small_crops_200:')
@@ -62,4 +89,7 @@ if __name__ == '__main__':
     print('Small_crops_1000:')
     for subdir in new_crop_subset_path_1000.iterdir():
         print('Number of samples in class {}: {}'.format(subdir.stem,len([item for item in subdir.glob('*')])))
+
+    is_there_test_leakage(new_crop_subset_path_200)
+    is_there_test_leakage(new_crop_subset_path_1000)
 
