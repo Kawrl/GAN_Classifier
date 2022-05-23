4189a71198cd96f0aaafd3d51f8d9611900f3b3a
Karl <karl.griphammar@gmail.com>
2022-05-23 11:52:37 +0200

    New assertion

--- count_resnet.py
+++ count_resnet.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 rest_set_path = Path('/data/rest_set')
-full_set_path = Path('/data/crops')
+test_set_path = Path('/data/test_crops')
 total_=0
 for label in rest_set_path.iterdir():
     label_files = [f for f in label.glob('*')]
@@ -11,12 +11,17 @@ for label in rest_set_path.iterdir():
 print(f'In total: {total_} files')
 
 all_rest_files = rest_set_path.glob('*/*.tiff')
-all_files = full_set_path.glob('*/*.tiff')
+all_files = test_set_path.glob('*/*.tiff')
 assert len(list(all_rest_files))==total_, "Something wrong with file counting?"
 
 all_rest_file_names = [f.name for f in all_rest_files]
 full_file_names = [f.name for f in all_files]
+print(len(all_rest_file_names))
+print(len(full_file_names))
 
 assert len(list(set(all_rest_file_names) & set(full_file_names))) == 0, \
         f"Intersection of train and test files: {list(set(all_rest_file_names) & set(full_file_names))}"
 
+for f in all_rest_file_names:
+    if f in full_file_names:
+        print(f)
